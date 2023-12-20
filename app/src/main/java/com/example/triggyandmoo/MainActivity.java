31c41476ee26b86c803ccd051b5277d3c2719701
Ohlittlejiji <115026133+Ohlittlejiji@users.noreply.github.com>
2023-12-20 22:53:58 +0700

    final(มั้ง) มันเอ๋ออยุ่

--- app/src/main/java/com/example/triggyandmoo/MainActivity.java
+++ app/src/main/java/com/example/triggyandmoo/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.triggyandmoo;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -12,79 +13,75 @@ public class MainActivity extends AppCompatActivity {
     private MediaPlayer backgroundMusic;
     private static final String PREF_KEY_BACKGROUND_MUSIC_PLAYING = "background_music_playing";
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-            // ตรวจสอบสถานะการเล่นเพลงใน SharedPreferences
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-            boolean isBackgroundMusicPlaying = prefs.getBoolean(PREF_KEY_BACKGROUND_MUSIC_PLAYING, false);
+        // ตรวจสอบสถานะการเล่นเพลงใน SharedPreferences
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isBackgroundMusicPlaying = prefs.getBoolean(PREF_KEY_BACKGROUND_MUSIC_PLAYING, false);
 
-            if (backgroundMusic == null || !backgroundMusic.isPlaying()) {
-                playBackgroundMusic(R.raw.sound2);
-            }
-
-            // เริ่มเล่นเพลงถ้าสถานะบอกว่ากำลังเล่น
-            if (isBackgroundMusicPlaying) {
-                backgroundMusic.start();
-            }
+        if (backgroundMusic == null) {
+            playBackgroundMusic(R.raw.sound2);
         }
 
-        private void playBackgroundMusic(int resourceId) {
-            if (backgroundMusic != null) {
-                backgroundMusic.release();
-            }
-            backgroundMusic = MediaPlayer.create(this, resourceId);
-            backgroundMusic.setLooping(true);
+        // เริ่มเล่นเพลงถ้าสถานะบอกว่ากำลังเล่น
+        if (isBackgroundMusicPlaying && !backgroundMusic.isPlaying()) {
             backgroundMusic.start();
-
-            // บันทึกสถานะการเล่นเพลงใน SharedPreferences
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-            SharedPreferences.Editor editor = prefs.edit();
-            editor.putBoolean(PREF_KEY_BACKGROUND_MUSIC_PLAYING, true);
-            editor.apply();
         }
+    }
 
-        private void stopBackgroundMusic() {
-            if (backgroundMusic != null) {
-                backgroundMusic.release();
-                backgroundMusic = null;
-
-                // เคลียร์สถานะการเล่นเพลงใน SharedPreferences
-                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-                SharedPreferences.Editor editor = prefs.edit();
-                editor.putBoolean(PREF_KEY_BACKGROUND_MUSIC_PLAYING, false);
-                editor.apply();
-            }
+    private void playBackgroundMusic(int resourceId) {
+        if (backgroundMusic != null) {
+            backgroundMusic.release();
         }
+        backgroundMusic = MediaPlayer.create(this, resourceId);
+        backgroundMusic.setLooping(true);
+        backgroundMusic.start();
 
-        @Override
-        protected void onDestroy() {
-            super.onDestroy();
-            stopBackgroundMusic();
+        // บันทึกสถานะการเล่นเพลงใน SharedPreferences
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean(PREF_KEY_BACKGROUND_MUSIC_PLAYING, true);
+        editor.apply();
+    }
+
+    private void stopBackgroundMusic() {
+        if (backgroundMusic != null && backgroundMusic.isPlaying()) {
+            backgroundMusic.pause();
         }
+    }
 
-        public void startGame(View view) {
-            stopBackgroundMusic();
-            GameView gameView = new GameView(this);
-            setContentView(gameView);
-            playBackgroundMusic(R.raw.sound);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopBackgroundMusic();
+        if (backgroundMusic != null) {
+            backgroundMusic.release();
+            backgroundMusic = null;
         }
+    }
 
-        @Override
-        protected void onResume() {
-            super.onResume();
-            if (backgroundMusic != null && !backgroundMusic.isPlaying()) {
-                backgroundMusic.start();
-            }
+    public void startGame(View view) {
+        stopBackgroundMusic();
+        GameView gameView = new GameView(this);
+        setContentView(gameView);
+        playBackgroundMusic(R.raw.sound);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (backgroundMusic != null && !backgroundMusic.isPlaying()) {
+            backgroundMusic.start();
         }
+    }
 
-        @Override
-        protected void onPause() {
-            super.onPause();
-            stopBackgroundMusic();
-        }
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopBackgroundMusic();
+    }
 }
