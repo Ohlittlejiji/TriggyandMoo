4680119c3e9d837b2abc40f0a9fd1a0e733dcba5
Chongchom <Pimma.dak99@gmail.com>
2023-12-21 12:22:46 +0700

    เพิ่มinfo

--- app/src/main/java/com/example/triggyandmoo/MainActivity.java
+++ app/src/main/java/com/example/triggyandmoo/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.triggyandmoo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer backgroundMusic;
     private static final String PREF_KEY_BACKGROUND_MUSIC_PLAYING = "background_music_playing";
+    private Dialog infoDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,5 +93,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         stopBackgroundMusic();
+    }
+
+
+    public void showInfoDialog(View view) {
+        // Create a new dialog
+        final Dialog infoDialog = new Dialog(this);
+
+        // Set the layout for the dialog
+        infoDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        infoDialog.setContentView(R.layout.info_dialog);
+
+        // Set the width to match parent and height to wrap content
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.copyFrom(infoDialog.getWindow().getAttributes());
+        layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        infoDialog.getWindow().setAttributes(layoutParams);
+
+        // Access the Close button and set its click listener
+        Button btnClose = infoDialog.findViewById(R.id.btnClose);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                infoDialog.dismiss(); // Dismiss the dialog when the Close button is clicked
+            }
+        });
+
+        // Show the dialog
+        infoDialog.show();
+    }
+
+
+
+
+
+
+
+
+    public void closeInfoDialog(View view) {
+        // ปิดป็อปอัพ (ถูกเรียกจากปุ่ม "Close")
+        if (infoDialog != null && infoDialog.isShowing()) {
+            infoDialog.dismiss();
+        }
     }
 }
